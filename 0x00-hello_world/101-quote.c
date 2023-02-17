b33aad9dfc7136135f6c7835e2e6a05cf944bcfd
DrHertz <k.bediako313@gmail.com>
2023-02-17 18:11:03 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 
 	return (1);
 }
