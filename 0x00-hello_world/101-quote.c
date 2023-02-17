8a35776ca3b64db001517879a4de9980f086d24f
DrHertz <k.bediako313@gmail.com>
2023-02-17 18:30:11 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+/*
+ * File: 101-quote.c
+ * Auth: Richard Puni
+ */
+
 #include <unistd.h>
+
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * Return: Always 1 (Success)
+ * Return: Always 1.
  */
 int main(void)
 {
-	fput("and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
 	return (1);
 }
