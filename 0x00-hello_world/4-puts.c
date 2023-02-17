176d92544174030b360afa31671b8c204e24bef7
DrHertz <k.bediako313@gmail.com>
2023-02-17 00:49:24 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-put("Programming is like building a multilingual puzzle\n")
+#include <stdio.h>
+/** 4=puts.c 
+ * Print programming is like buildhing a multilingual puzzle
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+       return(0);
+}
