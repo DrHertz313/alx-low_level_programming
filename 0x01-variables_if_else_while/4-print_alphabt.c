b87a197efbe042c84e62fd934c4903676cb294aa
DrHertz <k.bediako313@gmail.com>
2023-02-18 16:58:08 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int  main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
