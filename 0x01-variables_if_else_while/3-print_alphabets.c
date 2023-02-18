72e7674f6cc23972ceeb77f5ea9728e21a15904a
DrHertz <k.bediako313@gmail.com>
2023-02-18 15:51:47 +0000

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 'a'; ch = 'b'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	for (ch = 'A'; ch = 'B'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
 	putchar('\n');
 		return (0);
