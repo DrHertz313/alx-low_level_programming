b2b36358e99a3713c3bdf35d4cba79368d842d32
DrHertz <k.bediako313@gmail.com>
2023-02-18 22:05:19 +0000

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,12 +5,11 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char ch;
 
-	for (ch = 'z'; ch >= 'z'; ch--)
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
 	}
