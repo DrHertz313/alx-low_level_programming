cb5560e918e0e25a7beef2034eabcabfdde80182
DrHertz <k.bediako313@gmail.com>
2023-02-19 00:39:10 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,12 +20,12 @@ int main(void)
 				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
-					putchar('.');
+					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
