b193ceb3572e01147d8ccf6f9552ce1dca4a5be9
DrHertz <k.bediako313@gmail.com>
2023-02-21 18:54:33 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print _putchar
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char word[] = "_putchar";
+
+	for (word = _putchar;)
+	{
+		putchar(word);
+	}
+	putchar('\n');
+	return (0);
+}
+
