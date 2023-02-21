8fe8e95b1f32874bc0d1195ecfa9097da4e747ba
DrHertz <k.bediako313@gmail.com>
2023-02-21 21:06:00 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,15 @@
  */
 int main(void)
 {
-	char word = "_putchar";
-
-	for (word = _putchar;)
-	{
-		putchar(word);
-	}
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 	putchar('\n');
-	return (0);
+	return (0)
 }
 
