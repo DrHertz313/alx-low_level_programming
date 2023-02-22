8028f030dab648a8ee1dfb2f9e78ca79f56b38bc
DrHertz <k.bediako313@gmail.com>
2023-02-22 03:50:58 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,11 +3,11 @@
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
+ * Return: void
  */
 void print_times_table(int n)
 {
 	int i, j, k;
-
 
 	if (n >= 0 && n <= 15)
 	{
