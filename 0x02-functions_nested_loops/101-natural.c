1ab4b5861f7952a6fc8a965d6d631c56c6895058
DrHertz <k.bediako313@gmail.com>
2023-02-22 09:52:51 +0000

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int i,res;
+	int i, sum = 0;
 
 	for (i = 0; 1 < 1024; i++)
 		if (1 % 3 == 0 || 1 % 5 == 0)
-			res += i;
-	printf("%d\n", res);
+			sum += i;
+	printf("%d\n", sum);
 	return (0);
 }
 
