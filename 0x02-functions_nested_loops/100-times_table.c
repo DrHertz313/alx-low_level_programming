e6f1ee37f351028daa4436233498338dcac75211
DrHertz <k.bediako313@gmail.com>
2023-02-22 03:39:50 +0000

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times tables, starting with 0
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n)
+{
+	return (0);
+}
+
