4b8c1cbee36e2ba701fa563163e1428529cbb3c3
DrHertz <k.bediako313@gmail.com>
2023-02-24 02:14:48 +0000

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,13 @@
 /**
  * print_numbers - function that peints numbers, from 0 to 9
  * followed by a new line
- * Return: 0-9
+ * Return: 0-9 followed by a new line
  */
 void print_numbers(void);
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		_putchar(i);
-
+		_putchar(i + '0');
 	_putchar('\n');
 }
