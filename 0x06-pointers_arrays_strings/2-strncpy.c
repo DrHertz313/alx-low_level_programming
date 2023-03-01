a492a43c2e7006c540237e0219dcb83cb49429f5
DrHertz <k.bediako313@gmail.com>
2023-03-01 22:34:42 +0000

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,13 +6,13 @@
  * @dest: copy to
  * @src: copy from
  * @n: input number of char
- * Return: a copied string
+ * Return: Always 0 (Success)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
 
-	for (a = 0; dest[a] != '\0'; a++)
+	for (a = 0; src[a] != '\0'; a++)
 		if (a < n)
 			dest[a] = src[a];
 	while (a < n)
