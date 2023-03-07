e4f793421d9ca87c3fa827d54249db57afa3dcf6
DrHertz <k.bediako313@gmail.com>
2023-03-07 03:11:42 +0000

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - gets the leagth of a prefix substring
+ * @s: segment to return bytes from
+ * @accept: the bytes to include
+ * Return: the number of bytes in the initial segment of @s
+ * which consists only of bytes from @accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
+		else
+			return (c);
+	}
+	return (c);
+}
