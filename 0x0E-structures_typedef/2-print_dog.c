fd194a5bf57f0450995409fe45dc515066979d6d
DrHertz <k.bediako313@gmail.com>
2023-03-21 22:06:45 +0000

    Update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,12 +5,12 @@
  * print_dog - prints a struct dog
  * @d: structs dog to print
  */
-void print_dog(struct dog*d)
+void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
 
-	if (d->name ==NULL)
+	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
