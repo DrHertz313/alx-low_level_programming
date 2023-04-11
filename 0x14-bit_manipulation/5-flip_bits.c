952ffa454ee9aff45711ae2f14c47dcde0d148d8
DrHertz <k.bediako313@gmail.com>
2023-04-11 08:16:07 +0000

    update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,15 +9,15 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int xor = n ^ m;
-    unsigned int count = 0;
+	unsigned long int xor = n ^ m;
+	unsigned int count = 0;
 
-    while (xor)
-    {
-        count += xor & 1;
-        xor >>= 1;
-    }
+	while (xor)
+	{
+		count += xor & 1;
+		xor >>= 1;
+	}
 
-    return (count);
+	return (count);
 }
 
